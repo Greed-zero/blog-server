1db748ba4add179231d5fc6ffe61ab101d0f3535
ght <1311325183@qq.com>
2022-05-20 19:05:21 +0800

    接受前端传来的图片完成

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 //index.js
 const Koa= require("koa");
+const fs = require("fs")
 const router = require("koa-router")();
 const app = new Koa();
 const articleModel = require("./models/article")
-const bodyParser = require('koa-bodyparser')
 const tagsModel = require("./models/tags")
 const userModel = require("./models/user")
 const cors = require("koa2-cors")
+const koaBody = require('koa-body');
 
+app.use(require('koa-static')(__dirname + '/assets'))
 app.use(cors())
-app.use(bodyParser())
+// app.use(bodyParser())
+app.use(koaBody({
+    multipart: true,
+    enableTypes: ['json', 'form', 'text'],
+    // formidable: {
+    //     maxFileSize: 2000 * 1024 * 1024    // 设置上传文件大小最大限制，默认2M
+    // }
+}))
 app.use(router.routes());
+
+
 
 app.listen(8080);
 console.log('server running at http://localhost:8080')
@@ -44,6 +55,27 @@ router.post("/article/add",async ctx=>{
         msg:"Add  article  success"
     }
 })
+router.post("/article/uploadimg",koaBody(),async ctx =>{
+    var files = ctx.request.files
+    // console.log(files)
+    const reader = fs.createReadStream(files['image']['filepath']);
+    // console.log(reader)
+    let filePath = `assets/blog-image/${files['image']['newFilename']}.jpg`;
+    console.log('filePath:',filePath)
+    let remotefilePath = `http://localhost:8080/blog-image` + `/${files['image']['newFilename']}.jpg`;
+    console.log(remotefilePath)
+     // 创建可写流
+     const upStream = fs.createWriteStream(filePath);
+     console.log(upStream)
+     // 可读流通过管道写入可写流
+     reader.pipe(upStream);
+    ctx.body = {
+        url: remotefilePath,
+        code:200,
+        msg:"uploadimg  article  success"
+    }
+}
+)
 router.post("/article/delete",async ctx=>{
     deleteAll = ctx.request.body.deleteAll
     if(deleteAll){
